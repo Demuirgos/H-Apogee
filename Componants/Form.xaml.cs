34c6463a656d61dff63c0725b92e87dc94d507b8
DemuirGos <59487204+DemuirGos@users.noreply.github.com>
2021-02-24 00:14:11 +0100

    Initial Commit

---
+++ Componants/Form.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+namespace GenieLogicielProject.Componants
+{
+    /// <summary>
+    /// Interaction logic for Form.xaml
+    /// </summary>
+    public partial class Form : Page
+    {
+        FormData Model = new FormData();
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private void FirstNameBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Model.FirstName = (sender as TextBox).Text;
+        }
+
+        private void DemandeBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Model.Request = (sender as ComboBox).SelectedIndex switch
+            {
+                1 => FormData.RequestType.Bulletin,
+                _ => FormData.RequestType.Attestation,
+            };
+        }
+
+        private void LastNameBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Model.LastName = (sender as TextBox).Text;
+        }
+
+        private void CINBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Model.CIN = (sender as TextBox).Text;
+        }
+
+        private void ApogeeBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Model.Id = Convert.ToInt64((sender as TextBox).Text);
+        }
+
+        private void EmailBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Model.Email = (sender as TextBox).Text;
+        }
+    }
+}
---
+++ Componants/FormData.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace GenieLogicielProject.Componants
+{
+    class FormData
+    {
+        public enum RequestType
+        {
+            Attestation,
+            Bulletin
+        }
+        string _email;
+        public string Email
+        {
+            get => _email; 
+            set
+            {
+                if (Regex.Match(value, "[0-9A-Za-z.]+@[a-zA-Z]+[.][a-zA-Z]+").Success)
+                {
+                    _email = value;
+                }
+                else
+                {
+                    throw new Exception("Email Invalid");
+                }
+            }
+        }
+
+        public string FirstName
+        { get; set; }
+
+        public string LastName
+        { get; set; }
+
+        public long Id
+        { get; set; }
+
+        public string CIN
+        { get; set; }
+
+        public DateTime Date
+        { get; set; }
+
+        public RequestType Request
+        { get; set; }
+    }
+}
